4c27b6ad5c4aa558fee037af81045e3c2dc94e7a
Juan Miguel Salvatierra <31824478+juanmiguelsalvatierra@users.noreply.github.com>
2025-02-22 13:25:02 +0100

    Mobile version

--- main.js
+++ main.js
@@ -26,3 +26,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const drawer = document.getElementById('drawer');
+    const closeDrawer = document.querySelector('.close-drawer');
+  
+    navToggle.addEventListener('click', function() {
+      drawer.classList.toggle('open');
+    });
+
+    closeDrawer.addEventListener('click', function() {
+      drawer.classList.remove('open');
+    });
+
+    const expandButton = document.getElementById('expandButton');
+    const expandableSection = document.getElementById('expandableSection');
+
+    expandButton.addEventListener('click', function() {
+        expandableSection.classList.toggle('hidden');
+    });
+});
